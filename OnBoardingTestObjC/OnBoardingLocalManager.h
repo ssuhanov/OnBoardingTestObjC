942d8bec97c3a15c2dddd2a484e026fa1de7e7f2
Serge Sukhanov <ssuhanov@icloud.com>
2017-08-22 10:58:32 +0300

    Add local manager for OnBoarding

---
+++ OnBoardingTestObjC/OnBoardingLocalManager.h
@@ -0,0 +1,15 @@
+//
+//  OnBoardingLocalManager.h
+//  OnBoardingTestObjC
+//
+//  Created by Serge Sukhanov on 8/22/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OnBoardingLocalManager : NSObject
+
+- (void)setFlagOnBoardingCompleted;
+
+@end
---
+++ OnBoardingTestObjC/OnBoardingLocalManager.m
@@ -0,0 +1,19 @@
+//
+//  OnBoardingLocalManager.m
+//  OnBoardingTestObjC
+//
+//  Created by Serge Sukhanov on 8/22/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+#import "OnBoardingLocalManager.h"
+
+@implementation OnBoardingLocalManager
+
+- (void)setFlagOnBoardingCompleted {
+    // some logic for saving onBoardingCompleted flag
+    // maybe use UserDefaults.standard
+    NSLog(@"OnBoarding completed successfully");
+}
+
+@end
