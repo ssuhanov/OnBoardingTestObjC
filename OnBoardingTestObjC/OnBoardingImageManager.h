3deb7de9ca0c803fbef09dfbc695def8a16c1c18
Serge Sukhanov <ssuhanov@icloud.com>
2017-08-22 10:57:22 +0300

    Add image manager for OnBoarding

---
+++ OnBoardingTestObjC/OnBoardingImageManager.h
@@ -0,0 +1,15 @@
+//
+//  OnBoardingImageManager.h
+//  OnBoardingTestObjC
+//
+//  Created by Serge Sukhanov on 8/22/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OnBoardingImageManager : NSObject
+
+- (NSArray<NSString *> *)getImageQueue;
+
+@end
---
+++ OnBoardingTestObjC/OnBoardingImageManager.m
@@ -0,0 +1,21 @@
+//
+//  OnBoardingImageManager.m
+//  OnBoardingTestObjC
+//
+//  Created by Serge Sukhanov on 8/22/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+#import "OnBoardingImageManager.h"
+
+@implementation OnBoardingImageManager
+
+- (NSArray<NSString *> *)getImageQueue {
+    return @[@"OnBoarding1",
+             @"OnBoarding2",
+             @"OnBoarding3",
+             @"OnBoarding4",
+             @"OnBoarding5"];
+}
+
+@end
