cfd887e62406cca9d080cb1c0c0551cdf69323ea
Serge Sukhanov <ssuhanov@icloud.com>
2017-08-22 11:02:44 +0300

    Add imageQueue and showNextImage method to the presenter

--- OnBoardingTestObjC/OnBoardingPresenter.h
+++ OnBoardingTestObjC/OnBoardingPresenter.h
@@ -8,6 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+@protocol OnBoardingViewProtocol;
+@class OnBoardingLocalManager;
+
 @interface OnBoardingPresenter : NSObject
+
+@property (strong, nonatomic) NSMutableArray<NSString *> *imageQueue;
+
+- (instancetype)initWithImageQueue:(NSArray<NSString *> *)imageQueue;
+- (void)showNextImageWithView:(id<OnBoardingViewProtocol>)view;
+- (void)showNextImageWithView:(id<OnBoardingViewProtocol>)view andLocalManager:(OnBoardingLocalManager *)localManager;
 
 @end
--- OnBoardingTestObjC/OnBoardingPresenter.m
+++ OnBoardingTestObjC/OnBoardingPresenter.m
@@ -7,7 +7,32 @@
 //
 
 #import "OnBoardingPresenter.h"
+#import "OnBoardingViewProtocol.h"
+#import "OnBoardingLocalManager.h"
+#import "OnBoardingImageManager.h"
 
 @implementation OnBoardingPresenter
+
+- (instancetype)initWithImageQueue:(NSArray<NSString *> *)imageQueue {
+    self = [super init];
+    if (self) {
+        self.imageQueue = [imageQueue mutableCopy];
+    }
+    return self;
+}
+
+- (instancetype)init
+{
+    self = [self initWithImageQueue:[[[[OnBoardingImageManager alloc] init] getImageQueue] mutableCopy]];
+    return self;
+}
+
+- (void)showNextImageWithView:(id<OnBoardingViewProtocol>)view {
+    [self showNextImageWithView:view andLocalManager:[[OnBoardingLocalManager alloc] init]];
+}
+
+- (void)showNextImageWithView:(id<OnBoardingViewProtocol>)view andLocalManager:(OnBoardingLocalManager *)localManager {
+    
+}
 
 @end
