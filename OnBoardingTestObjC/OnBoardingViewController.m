7f99db4b0ce89ec4a14551eddaa74906b4e6b43e
Serge Sukhanov <ssuhanov@icloud.com>
2017-08-22 10:51:32 +0300

    Implement UI and module assembling

--- OnBoardingTestObjC/OnBoardingViewController.m
+++ OnBoardingTestObjC/OnBoardingViewController.m
@@ -7,31 +7,26 @@
 //
 
 #import "OnBoardingViewController.h"
+#import "OnBoardingPresenter.h"
 
 @interface OnBoardingViewController ()
+
+@property (strong, nonatomic) OnBoardingPresenter *presenter;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIButton *continueButton;
 
 @end
 
 @implementation OnBoardingViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    self.presenter = [[OnBoardingPresenter alloc] init];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)continueAction:(UIButton *)sender {
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
